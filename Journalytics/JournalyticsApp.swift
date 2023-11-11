677dc0ec5dab96193c3cc3131047d4c05cbb0705
Atonbara-Adora <the.atonbara@gmail.com>
2023-11-11 00:13:43 -0500

    Initial Commit

---
+++ Journalytics/JournalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalyticsApp.swift
+//  Journalytics
+//
+//  Created by Atonbara Diete-Koki on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
